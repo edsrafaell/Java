2b9d3abb05d15df77ecde96cfb353e644ec4a8ed
EdsonRafaelS <eds.rrafael@gmail.com>
2025-12-02 23:59:26 -0300

    .

--- JavaIntroducao/Documentação/Metodos/Metodos08Escopo.java
+++ JavaIntroducao/Documentação/Metodos/Metodos08Escopo.java
@@ -1,10 +1,10 @@
 public class Metodos08Escopo {
-    public class Main(String[] args){
-        int x = 10;
-    
+    public class Main {
+    int x = 5; // Class variable
+
     public static void main(String[] args) {
-    Main myObj = new Main();
-    System.out.println(myObj.x); // Accessible here
-  }
-}
+        Main myObj = new Main();
+        System.out.println(myObj.x); // Accessible here
+    }
+    }
 }
